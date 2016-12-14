7182b34fe5882a7162427cf863aeae6f1527cd2e
rongrongt <dy@rrtekiMacBook-Air.local>
2016-12-14 21:47:54 +0800

    轮播组件

--- wyslider.js
+++ wyslider.js
@@ -2,6 +2,9 @@ function Slider(opt){
 	this.images = opt.images;
 	this.container = opt.container;
 	this.show = opt.show||1;//
+	
+	this.currentIndex = 0;
+	this.count = this.images.length;
 	this.render();
 }
 
@@ -9,41 +12,43 @@ function Slider(opt){
 
 Slider.prototype.render = function(){
 	var images = this.images.length;
-	var str1 = '';
-	var str2 = '';
-	var str3 ='<div class=sl-box id="sl-box">'
+	
+	
+	var str1 ='<div class=sl-box id="sl-box" style="left:0">'
 	for(var i=0 ; i<images; i++){
 		
 		str1 +='<div class="off-banner" style="left:'+i*100+'%"><img src="'+this.images[i]+'" alt=""></div>';
 	}
-	for(var i=0 ; i<images; i++){
-		
-		 str2 +='<div class="off-banner" style="left:'+(i+images)*100+'%"><img src="'+this.images[i]+'" alt=""></div>';
-	}
+	str1 += '<div class="off-banner" style="left:'+i*100+'%"><img src="'+this.images[0]+'" alt=""></div>' 
 	
-	str3 += str1+str2+'</div>'
-	this.container.innerHTML = str3;
+	str1 += '</div>'
+	this.container.innerHTML = str1;
 	
 }
 
 
 Slider.prototype.get = function(){
 	var self = this;
-	//var aaa = document.querySelectorAll('.off-banner');
+	var aaa = document.querySelectorAll('.off-banner');
+	var length = aaa.length;
 	var inContainner = document.getElementById('sl-box');
-	setInterval(function(){
-		inContainner.style.left += parseInt(inContainner.style.left)+100+'%'; 
-	},2000);
 
+	setInterval(function(){
+		if(self.currentIndex == self.count){
+			inContainner.style.transitionProperty = 'none';
+			inContainner.style.left = '0';
+			self.currentIndex = 0;
+			setTimeout(function(){
+				inContainner.style.transitionProperty = 'left';
+				inContainner.style.left = (parseInt(inContainner.style.left) - 100) + '%';
+				self.currentIndex++;
+			}, 50);
+		}else{
+			inContainner.style.transitionProperty = 'left';
+			inContainner.style.left = (parseInt(inContainner.style.left) - 100) + '%';
+			self.currentIndex++;
+		}
+		
+	},5000);
 }
 
-// for(i=0;i<aaa.length;i++){
-		// 	if(parseInt(aaa[i].style.left) != -100){
-		// 		aaa[i].style.left =  parseInt(aaa[i].style.left)-100+'%';
-		// 	}else{
-		// 		aaa[i].style.transition = 'right 0s';
-		// 		aaa[i].style.left =  (aaa.length-2)*100+'%';
-
-		// 	}
-		// }
-		// //self.render()style="left:'+i*100+'%";
